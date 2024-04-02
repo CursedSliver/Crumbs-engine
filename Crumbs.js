ac9a3f15a205a02326464426367fa6e3f62ab75d
CursedSliver <162417685+CursedSliver@users.noreply.github.com>
2024-04-02 11:26:02 +0800

    Update Crumbs.js

--- Crumbs.js
+++ Crumbs.js
@@ -90,7 +90,7 @@ var Crumbs_Init_On_Load = function() {
 		} else if (Array.isArray(obj.behaviors)) {
 			let f = [];
 			for (let i in obj.behaviors) {
-				if (Array.isArray(obj.behaviors[i])) { f = f.concat(obj.behaviors[i]); } else { f = f.concat([obj.behaviors[i], {}]); }
+				if (Array.isArray(obj.behaviors[i])) { f = f.push(obj.behaviors[i]); } else { f = f.push([obj.behaviors[i], {}]); }
 			}
 			this.behaviors = f;
 		} else {
